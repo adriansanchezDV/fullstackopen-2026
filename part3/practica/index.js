ab92b0e652914a6116cbfdf61d53415b4cfe4193
Adrian Sanchez Huertas <adriansanchezhuertas26@gmail.com>
2026-06-02 20:34:48 +0200

    remove node_modules

--- part3/practica/index.js
+++ part3/practica/index.js
@@ -1,10 +1,9 @@
 const express = require('express')
-const app = express()
-
 const cors = require('cors')
 
-app.use(cors())
+const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 let notes = [
@@ -85,7 +84,7 @@ app.post('/api/notes', (request, response) => {
   response.json(note)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
