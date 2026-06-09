388dd80407528442fc904233ac888cb40192dd8a
Adrian Sanchez Huertas <adriansanchezhuertas26@gmail.com>
2026-06-09 14:24:13 +0200

    MongoDB notes backend

--- PRACTICA_BACKEND/index.js
+++ PRACTICA_BACKEND/index.js
@@ -1,62 +1,47 @@
+require('dotenv').config()
+
 const express = require('express')
 const cors = require('cors')
-
+const Note = require('./models/note')
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
 
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  },
-   {
-    id: "4",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
+
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello world !</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  const note = notes.find(note => note.id === id)
-  
-
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
-  }
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+     .catch(error => {
+      console.log(error)
+      response.status(500).end()
+    })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
+  Note.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
 })
-
 const generateId = () => {
   const maxId = notes.length > 0
     ? Math.max(...notes.map(n => Number(n.id)))
@@ -68,24 +53,22 @@ app.post('/api/notes', (request, response) => {
   const body = request.body
 
   if (!body.content) {
-    console.log('Content missing')
-    return response.status(400).json({ 
+    return response.status(400).json({
       error: 'content missing'
-    }) 
+    })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  }
+  })
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
