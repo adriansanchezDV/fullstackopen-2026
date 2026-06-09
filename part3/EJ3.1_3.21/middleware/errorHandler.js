788e81eb77cbe0c420c7a1d247dfeea8f5ed8177
Adrian Sanchez Huertas <adriansanchezhuertas26@gmail.com>
2026-06-09 16:36:42 +0200

    Add full

--- part3/EJ3.1_3.21/middleware/errorHandler.js
+++ part3/EJ3.1_3.21/middleware/errorHandler.js
@@ -5,7 +5,13 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).send({ error: 'malformatted id' })
   }
 
+  if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
   next(error)
 }
+
+module.exports = errorHandler
 
 module.exports = errorHandler
--- part3/EJ3.1_3.21/models/person.js
+++ part3/EJ3.1_3.21/models/person.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose')
 const personSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true
+    required: true,
+    minlength: 3
   },
   number: {
     type: String,
