dd97921dfe424820a41b5c33dce1d3b22f728657
Adrian Sanchez Huertas <adriansanchezhuertas26@gmail.com>
2026-07-14 13:48:38 +0200

    Practica de Redux

---
+++ PRACTICA_REDUX/src/main.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import App from './App'
+import store from './store'
+
+
+const root =
+ReactDOM.createRoot(
+document.getElementById('root')
+)
+
+
+
+const renderApp = () => {
+
+root.render(
+<App/>
+)
+
+}
+
+
+renderApp()
+
+
+store.subscribe(renderApp)
---
+++ part6/unicafeRedux/src/App.jsx
@@ -0,0 +1,18 @@
+import counterReducer from './reducers/counterReducer'
+import store from './store'
+
+const App = () => {
+  return (
+    <div>
+      <button onClick={() => store.dispatch({ type: 'GOOD' })}>good</button>
+      <button onClick={() => store.dispatch({ type: 'OK' })}>ok</button>
+      <button onClick={() => store.dispatch({ type: 'BAD' })}>bad</button>
+      <button onClick={() => store.dispatch({ type: 'RESET' })}>reset stats</button>
+      <div>good {store.getState().good}</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
+    </div>
+  )
+}
+
+export default App
